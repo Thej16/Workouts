34414bfb8e2da4f096f97393bea49f5931c8ccdb
Thejesh <tejs16@gmail.com>
2022-12-14 19:00:21 +0530

    updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '8d' })
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '9d' })
 }
 
 // login a user
